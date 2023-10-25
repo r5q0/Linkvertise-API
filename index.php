a110a0aa771fe6e66e925e63358d0f6dbe3a0375
raq0 <122687779+raq0@users.noreply.github.com>
2023-10-25 02:02:44 +0200

    added userid

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    if (strpos($path, '/r=')) {
+        $originalRequest = substr($path, 3);
+        $randomNumber = rand(0, 1000);
+        echo $randomNumber;
+        $redirectUrl = 'https://linkvertise.com/USERID/' . $randomNumber . '/dynamic?r=' . $originalRequest;
+        header('Location: ' . $redirectUrl, true, 302);
+        exit();
+    } else {
+        http_response_code(404);
+        header('Content-Type: text/plain');
+        echo '404 Not Found';
+    }
+} else {
+    http_response_code(405);
+}
+?>
